87d54647f680c96762c3b46afbdf8c382385ac47
Saad Bin Khalid <saadbkhalid666@gmail.com>
2025-09-19 20:00:56 +0500

    Code for model

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Model } from '../../components/model/model';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Model],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
