7507941f00a46bd4299f04700fe1b80df6c13e9e
Saad Bin Khalid <saadbkhalid666@gmail.com>
2025-10-02 10:36:49 +0500

    terms n condition page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Signup } from './pages/signup/signup';
 import { Login } from './pages/login/login';
 import { Otp } from './pages/otp/otp';
 import { Home } from './pages/home/home';
- 
+import { Terms } from './pages/terms/terms';
+
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' }, 
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'signup', component: Signup },
   { path: 'login', component: Login },
   { path: 'verify-otp', component: Otp },
   { path: 'home', component: Home },
-  { path: '**', redirectTo: 'signup' }
+  { path: 'terms', component: Terms },
+  { path: '**', redirectTo: 'signup' },
 ];
