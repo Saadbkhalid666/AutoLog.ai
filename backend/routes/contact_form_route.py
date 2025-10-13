8fd28a2c882bc4062e8d852f47a834116b046976
Saad Bin Khalid <saadbkhalid666@gmail.com>
2025-10-13 18:43:23 +0500

    contact route

--- backend/routes/contact_form_route.py
+++ backend/routes/contact_form_route.py
@@ -1,1 +1,12 @@
-from flask import jsonify, request #type:ignore
+from flask import jsonify, request, Blueprint #type:ignore
+from models.User import User
+
+contact_bp = Blueprint("form", __name__)
+
+@contact_bp.route("/contact", methods=['POST'])
+def submit_form():
+    data  = request.json
+    name = data.get('name')
+    email = data.get('email')
+    msg = data.get('message')
+    admin = User.query.filter_by()
