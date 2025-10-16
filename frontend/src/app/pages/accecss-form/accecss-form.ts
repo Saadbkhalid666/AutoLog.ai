79d6c4a0739c980ec900e7434af3cb328adfa7ae
Saad Bin Khalid <saadbkhalid666@gmail.com>
2025-10-16 08:56:04 +0500

    db access form

---
+++ frontend/src/app/pages/accecss-form/accecss-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accecss-form',
+  imports: [],
+  templateUrl: './accecss-form.html',
+  styleUrl: './accecss-form.css'
+})
+export class AccecssForm {
+
+}
