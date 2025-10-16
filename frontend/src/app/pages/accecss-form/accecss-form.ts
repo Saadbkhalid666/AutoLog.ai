66d90943f2862c6572bb905f9bbe00f4d055bad1
Saad Bin Khalid <saadbkhalid666@gmail.com>
2025-10-16 09:04:19 +0500

    access form

--- frontend/src/app/pages/accecss-form/accecss-form.ts
+++ frontend/src/app/pages/accecss-form/accecss-form.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-
+import { AdminService } from '../../services/admin.service';
 @Component({
-  selector: 'app-accecss-form',
-  imports: [],
-  templateUrl: './accecss-form.html',
-  styleUrl: './accecss-form.css'
+  selector: 'app-access-form',
+  templateUrl: './access-form.html'
 })
-export class AccecssForm {
+export class AdminLoginComponent {
+  email = '';
+  password = '';
+  errorMessage = '';
 
+  constructor(private authService: AdminService) {}
+
+  onLogin() {
+    this.authService.adminLogin({ email: this.email, password: this.password }).subscribe({
+      next: () => {
+        window.location.href = 'http://127.0.0.1:5000/admin'; // redirect to flask admin
+      },
+      error: (err) => {
+        this.errorMessage = err?.error?.error || 'Login failed';
+      }
+    });
+  }
 }
