46d5dcfc5198928e21bce956d3fc04dae66134cd
Saad Bin Khalid <saadbkhalid666@gmail.com>
2025-10-26 18:31:01 +0500

    done

--- frontend/src/app/sections/about/about.ts
+++ frontend/src/app/sections/about/about.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CarModel } from '../../components/car-model/car-model';
 
 @Component({
   selector: 'app-about',
   standalone:true,
-  imports: [CarModel],
+  imports: [],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
